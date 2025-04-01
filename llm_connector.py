718df031c4d767c8854ea4ff5d7b0e05778cde4a
Parsa <pheydari33@yahoo.com>
2025-04-01 20:12:28 +0330

    Update Ollama import

--- llm_connector.py
+++ llm_connector.py
@@ -1,7 +1,7 @@
-from langchain_community.llms import Ollama
+from langchain_ollama import OllamaLLM
 
 def get_llm(model_type):
     if model_type == "ollama":
-        return Ollama(model="llama3.2")
+        return OllamaLLM(model="llama3.2")
     else:
         raise ValueError("Invalid Model: Only 'ollama' is supported for now.")
